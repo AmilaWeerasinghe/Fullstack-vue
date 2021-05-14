516fe00c4074ce1dd6c85dad95c373253f1eb155
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-14 20:19:00 +0530

    Product Grid modularized

---
+++ fsv-front-end/src/components/ProductGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid-wrap">
+    <ProductGridList v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+<script>
+import ProductGridList from "./ProductGridList.vue";
+export default {
+  name: "ProductGrid",
+  components: {
+    ProductGridList,
+  },
+  props: ["products"],
+};
+</script>
+<style scoped>
+.grid-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
+</style>
--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,25 +1,20 @@
 <template>
   <div id="page-wrap">
-    <div class="grid-wrap">
-      <div v-for="product in products" class="product-item" v-bind:key="product.id">
-        <img v-bind:src="product.imageUrl" />
-        <h3 class="product-name">{{product.name}}</h3>
-        <p class="product-price">${{product.price}}</p>
-        <router-link v-bind:to="'/products/'+product.id">
-          <button>View details</button>
-        </router-link>
-      </div>
-    </div>
+    <ProductGrid :products="products" />
   </div>
 </template>
 <script>
 import { products } from "../fake-data";
+import ProductGrid from "../components/ProductGrid.vue";
 export default {
   name: "ProductsPage",
   data() {
     return {
       products,
     };
+  },
+  components: {
+    ProductGrid,
   },
 };
 </script>
@@ -30,35 +25,6 @@ export default {
   flex-wrap: wrap;
   justify-content: space-between;
   margin-top: 16px;
-}
-
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
-
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
 }
 </style>
 

c6708aa71574d5b015d6cefa2fb3901cb85af7f8
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-14 14:08:03 +0530

    product details page

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Products Page</h1>
+  <div id="page-wrap">
+    <div class="grid-wrap">
+      <div v-for="product in products" class="product-item" v-bind:key="product.id">
+        <img v-bind:src="product.imageUrl" />
+        <h3 class="product-name">{{product.name}}</h3>
+        <p class="product-price">${{product.price}}</p>
+        <router-link v-bind:to="'/products/'+product.id">
+          <button>View details</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { products } from "../fake-data";
 export default {
   name: "ProductsPage",
+  data() {
+    return {
+      products,
+    };
+  },
 };
 </script>
 

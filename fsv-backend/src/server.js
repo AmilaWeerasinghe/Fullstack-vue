a64619e5abd45a00881d3065d978bdda32e067b9
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-21 16:53:15 +0530

    post route for /hello end point

--- fsv-backend/src/server.js
+++ fsv-backend/src/server.js
@@ -1,9 +1,18 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
     res.send('Hello!');
+});
+
+//create a post route for /hello end point
+app.post('/hello',(req,res)=>{
+    res.send(`hello ${req.body.name} from /hello end point post route`);
+
 });
 
 app.listen(8000, () => {

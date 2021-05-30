93b403598386b20e5ea2232bc1b5c19ecf63c6ec
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-05-30 21:14:51 +0530

    mongo db server products retrive sucess

--- fsv-backend/src/server.js
+++ fsv-backend/src/server.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 //data
 
@@ -60,7 +61,7 @@ const products = [{
     imageUrl: '/images/shoes-8.jpg',
     averageRating: '5.0',
   }, {
-    id: '901',
+    id: '906',
     name: 'Plain Shoes',
     price: '54.00',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
@@ -103,8 +104,15 @@ app.use(bodyParser.json());
 
 
 //end point for all products
-app.get('/api/products',(req,res)=>{
+app.get('/api/products',async (req,res)=>{
+  const client=await MongoClient.connect(
+    'mongodb://localhost:27017',
+    {useNewUrlParser:true,useUnifiedTopology:true}
+  );
+  const db=client.db('vue-db');
+  const products=await db.collection('products').find({}).toArray();
     res.status(200).json(products);
+    client.close();
 });
 
 //end point for users cart items 

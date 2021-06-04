7b1c8e2af059a80949e5d6c4bd7888e974c773ca
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2021-06-04 17:53:26 +0530

    get end point for user cartItems

--- fsv-backend/src/server.js
+++ fsv-backend/src/server.js
@@ -116,8 +116,21 @@ app.get('/api/products',async (req,res)=>{
 });
 
 //end point for users cart items 
-app.get('/api/users/:userId/cart',(req,res)=>{
+app.get('/api/users/:userId/cart',async (req,res)=>{
+  const {userId}=req.params;
+  const client=await MongoClient.connect(
+    'mongodb://localhost:27017',
+    {useNewUrlParser:true,useUnifiedTopology:true}
+  );
+  const db=client.db('vue-db');
+  const user= await db.collection('users').findOne({id:userId});
+  if(!user) return res.status(404).json('Could not find the user');
+  const products=await db.collection('products').find({}).toArray();
+  const cartItemIds=user.cartItems;
+  const cartItems=cartItemIds.map(id=> products.find(product=>product.id===id));
+  
     res.status(200).json(cartItems);
+    client.close();
 });
 
 //end point for product details page
